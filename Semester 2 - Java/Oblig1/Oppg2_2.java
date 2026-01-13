0c14485670fe0428c818bc9bad76acb714b3be19
TitasBSP <titasbsp@gmail.com>
2026-01-13 17:52:47 +0100

    ferdig?

--- Semester 2 - Java/Oblig1/Oppg2_2.java
+++ Semester 2 - Java/Oblig1/Oppg2_2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; 
+import java.util.Scanner; // Et verktøy for å lese input
 
 public class Oppg2_2 {
     public static void main(String[] args) { 
@@ -10,10 +10,10 @@ public class Oppg2_2 {
 
         try {
             weight = Integer.parseInt(text);
-            System.out.println("Your weight on the moon is around: " + (weight * 0.165) + " Kilograms.");
+            System.out.println("Your weight on the moon is around: " + (weight * 0.165) + " Kilograms."); // Kalkulerer vekten ved å ta omtrent 1/6 av input.
 
         } catch (NumberFormatException e) {
-            System.out.println("Error: Can't convert weight due to an invalid number!");
+            System.out.println("Error: Can't convert weight due to an invalid number!"); // Fanger eventuelle feil der input inkluderer strenger.
         }
     }
 }
--- Semester 2 - Java/Oblig1/Oppg2_3.java
+++ Semester 2 - Java/Oblig1/Oppg2_3.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Oppg2_3 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        Planet mars = new Planet( "Mars", 3889.5, 6.39E23);
+        Planet mars = new Planet( "Mars", 3889.5, 6.39E23); // 
         Planet jupiter = new Planet( "Jupiter", 71492, 1.93E27);
         Planet mercury = new Planet("Mercury", 2440, 3.3E23 );
 
-        Planet[] planets;
+        Planet[] planets; // Lager array som lagrer flere planeter
         String[] planetNames;
 
         int i = 0;
-        planets = new Planet[42]; // The meaning of life is 42, one must not question it.
-        planetNames = new String[42];
+        planets = new Planet[42]; // Meningen av livet er 42, man skal ikke utspørre det.
+        planetNames = new String[42]; // Siden vi kan ikke printe planeten i terminalen så tar vi bare navnet.
 
         System.out.println("Planeten " + mars.name + " har en radius paa " + mars.radius + " og en masse paa " + mars.mass + " kg");
         System.out.println("Planeten " + jupiter.name + " har en radius paa " + jupiter.radius + " og en masse paa " + jupiter.mass + " kg");
@@ -23,7 +23,7 @@ public class Oppg2_3 {
             System.out.println("\nVelg en handling:\n[1 - Opprette en ny planet] / [2 - Liste eksisterende planeter] / [3 - Slette en planet] / [4 - Oppdatere en planet]");
             String choice = read.nextLine();
 
-            switch(choice) {
+            switch(choice) { // Switch casen går gjennom de forskjellige valgene basert på brukerens input.
                 
                 case "1" -> {
                     System.out.println("\nNaa kan du lage din egen planet!");
@@ -42,7 +42,7 @@ public class Oppg2_3 {
                     try {
                         radius = Double.parseDouble(givenRadius);
                         mass = Double.parseDouble(givenMass); 
-                        Planet planet0 = new Planet(name, radius, mass);
+                        Planet planet0 = new Planet(name, radius, mass); // Oppretter en ny objekt basert på brukerens verdier.
                         
                         System.out.println("\nPlaneten " + planet0.name + " har en radius paa " + planet0.radius + " og en masse paa " + planet0.mass + " kg");
                         System.out.println("Planeten har blitt lagt til i posisjonen: " + i);
@@ -50,19 +50,19 @@ public class Oppg2_3 {
                         planets[i] = planet0;
                         planetNames[i] = planet0.name;
 
-                        i++;
+                        i++; // Øker plassen der nye planeter legges slik at den ikke skriver over eksisterende planeter.
                     } catch (NumberFormatException e) {
-                        System.out.println("Error: Can't convert due to an invalid number!");
+                        System.out.println("Error: Can't convert due to an invalid number!"); // Fanger ugyldige verdier som strenger i radius eller vekt.
                     }
 
                     break;
                 }
 
-                case "2" -> {
+                case "2" -> { // Vi sjekker om listen er tom for hver gang vi skal gjøre en operasjon som krever at vi ser listen.
                     System.out.println("\nListen med eksisterende planeter:");
                     if (planetNames[0] == null) {
                         System.out.println("\nDenne listen er tom.");
-                    } else {
+                    } else { // Ellers skriver vi ut planetene slik det skal gjøres.
                         for (String planetName : planetNames) {
                             if (planetName != null) {
                                 System.out.println(planetName);
@@ -73,7 +73,7 @@ public class Oppg2_3 {
                     break;
                 }
 
-                case "3" -> {
+                case "3" -> { 
                     if (planetNames[0] == null) {
                         System.out.println("\nDenne listen er tom.");
                     } else {
@@ -87,12 +87,14 @@ public class Oppg2_3 {
                         String del = read.nextLine();
                         del = del.toLowerCase();
 
-                        for (int j = 0; j < planetNames.length; j++) {
+                        for (int j = 0; j < planetNames.length; j++) { // Vi null'ifyer objekten dersom den finnes på listen, samme logikk gjelder for resten av koden.
                             if (planetNames[j] != null && planetNames[j].equals(del)) {
                                 planetNames[j] = null;
                                 planets[j] = null;
                                 System.out.println("\nPlaneten " + del + " har blitt slettet fra listen.");
                                 break;
+                            } else {
+                                System.out.println("\nError: Planeten ikke funnet.");
                             }
                         }
                     }
@@ -131,25 +133,34 @@ public class Oppg2_3 {
                                     case "2" -> {
                                         System.out.println("Hva blir den nye radiusen?: ");
                                         String changeRadius = read.nextLine();   
-                                        double newChangeRadius = Double.parseDouble(changeRadius);
-
-                                        System.out.println("Radiusen til " + planetNames[j] + " har blitt endret til " + changeRadius);
-                                        planets[j].radius = newChangeRadius;
+                                        
+                                        try {
+                                            double newChangeRadius = Double.parseDouble(changeRadius);
+                                            System.out.println("Radiusen til " + planetNames[j] + " har blitt endret til " + changeRadius);
+                                            planets[j].radius = newChangeRadius;
+                                        } catch (NumberFormatException e) {
+                                            System.out.println("Error: Can't convert due to an invalid number!");
+                                        }
                                         break;
                                     }
 
                                     case "3" -> {
                                         System.out.println("Hvor mye skal planeten veie?: ");
-                                        String changeMass = read.nextLine();   
-                                        double newChangeMass = Double.parseDouble(changeMass);
-
-                                        System.out.println("Massen til " + planetNames[j] + " har blitt endret til " + changeMass);
-                                        planets[j].mass = newChangeMass;
+                                        String changeMass = read.nextLine();
+                                        
+                                        try {
+                                            double newChangeMass = Double.parseDouble(changeMass);
+                                            System.out.println("Massen til " + planetNames[j] + " har blitt endret til " + changeMass);
+                                            planets[j].mass = newChangeMass;
+                                        } catch (NumberFormatException e) {
+                                            System.out.println("Error: Can't convert due to an invalid number!");
+                                        }
+                                        
                                         break;
                                     }
                                 }
-                                
-                                break;
+                            } else if (planetNames[j] != null) {
+                                System.out.println("\nError: Planeten ikke funnet.");
                             }
                         }
                     }
