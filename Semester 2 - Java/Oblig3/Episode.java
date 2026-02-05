5d92ec8a22d5d473575e02b4d62caa8898fb7215
TitasBSP <titasbsp@gmail.com>
2026-02-05 11:46:40 +0100

    begin oblig3

---
+++ Semester 2 - Java/Oblig3/Episode.java
@@ -0,0 +1,30 @@
+public class Episode extends Production {
+    private int episodeNumber;
+    private int seasonNumber;
+
+    public int getEpisodeNumber() {return episodeNumber;}
+    public int getSeasonNumber() {return seasonNumber;}   
+
+    public void setEpisodeNumber(int episodeNumber) {this.episodeNumber = episodeNumber;}
+    public void setSeasonNumber(int seasonNumber) {this.seasonNumber = seasonNumber;}
+
+    @Override
+    public String toString() {
+        return "\nEpisode Title: " + super.getTitle()
+                + "\nEpisode Number: " + episodeNumber
+                + "\nSeason Number: " + seasonNumber
+                + "\nRuntime: " + super.getRuntimeInMinutes();
+    }
+
+    public Episode(String title, int episodeNumber, int seasonNumber, int runtime) {
+        this.title = title;
+        this.episodeNumber = episodeNumber;
+        this.seasonNumber = seasonNumber;
+        this.runtime = runtime;
+    }
+
+    public Episode(String title, int episodeNumber, int seasonNumber) {
+        this(title, episodeNumber, seasonNumber, 0);
+    }
+}
+
---
+++ Semester 2 - Java/Oblig3/Production.java
@@ -0,0 +1,12 @@
+public class Production {
+    private String title;
+    private int runtimeInMinutes;
+
+    public String getTitle() {return title;}
+    public int getRuntimeInMinutes() {return runtimeInMinutes;}
+
+    public void setTitle(String title) {this.title = title;}
+    public void setRuntimeInMinutes(int runtimeInMinutes) {this.runtimeInMinutes = runtimeInMinutes;}
+
+    
+}
